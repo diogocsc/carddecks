61f7449da96dd06b6b988d99fb508e8a38236f3c
Diogo Cordeiro <diogo@diogocordeiro.pt>
2022-12-31 13:19:12 +0000

    [IMP] Added description field

--- models/carddecks_deck.py
+++ models/carddecks_deck.py
@@ -7,7 +7,7 @@ class Deck(models.Model):
     _description = "Deck"
     name = fields.Char(required=True)
     cards = fields.One2many("carddecks.card", "deck", "Cards")
-
+    description = fields.Char()
     total_cards = fields.Integer(compute="_compute_total_cards")
 
     def _compute_total_cards(self):
